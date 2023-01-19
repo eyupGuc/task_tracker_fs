747ddaaea14221976767809f4af0d22e77bd7106
eyupGuc <meydanguresi@gmail.com>
2023-01-19 09:06:33 +0300

    class views

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,19 @@
 
 from django.urls import path
-from .views import todo_list_create,todo_home
-
+from .views import (
+    todo_list_create,
+    todo_home,
+    todo_detail,
+    Todo,
+    TodoSerializer
+    
+    )
 
 
 urlpatterns = [
-    path("",todo_home),
-    path("list-create/", todo_list_create)
+    path("", todo_home),
+    # path("list-create/", todo_list_create),
+    # path("detail/<int:id>", todo_detail),
+    path("list-create/", Todo.as_view()),
+    path("detail/<int:id>", todo_detail.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 from .models import Todo
 from .serializers import TodoSerializer
+
+# ListCreateAPIView listeleme create etmek
+# RetrieveUpdateDestroyAPIView single obje çekme silme update
 
 
 @api_view()
@@ -28,20 +32,35 @@ def todo_list_create(request):
 
 
 @api_view(['GET', 'DELETE', 'PUT'])
-def todo_detail(request):
-    todo = get_list_or_404(Todo, id=id)
-    if request.method == 'GET':
-        # todo = Todo.objects.get()
+def todo_detail(request, id):
+    todo = get_object_or_404(Todo, id=id)
 
+    if request.method == 'GET':
+        # todo = Todo.objects.get(id=id)
         serializer = TodoSerializer(todo)
         return Response(serializer.data)
-    
-    
+
     elif request.method == 'PUT':
-        serializer = TodoSerializer(data=request.data,  instance=todo)
+        serializer = TodoSerializer(data=request.data, instance=todo)
         if serializer.is_valid():
             serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
         todo.delete()
-        return Response({'message': 'todo deleted successfully'})
+        return Response({'message': 'todo deleted succesfully'})
+    
+    
+    ###################### CLASS BASE VİEWS  ########################
+    
+    class Todos(ListCreateAPIView):
+        queryset = Todo.objects.filter(is_done=False)
+        serializer_class = TodoSerializer
+        
+        
+    class TodoDetail(RetrieveUpdateDestroyAPIView):
+        queryset = Todo.objects.all(is_done=False)
+        serializer_class = TodoSerializer
+        
+    
