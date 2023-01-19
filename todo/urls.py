1c3b7efea94a656d2db65e962b6e6de29d815e7b
eyupGuc <meydanguresi@gmail.com>
2023-01-19 07:59:25 +0300

    move

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
-from .views import todo_list_create
+from .views import todo_list_create,todo_home
+
 
 
 urlpatterns = [
+    path("",todo_home),
     path("list-create/", todo_list_create)
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+
 from .models import Todo
 from .serializers import TodoSerializer
+
+@api_view()
+def todo_home(request):
+    return Response({'home': 'This is todo home page'})
 
 @api_view(['GET','POST'])
 def todo_list_create(request):
@@ -18,6 +23,24 @@ def todo_list_create(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET','DELETE','PUT'])
+def todo_detail(request):
+    todo = get_list_or_404(Todo,id=id)
+    if request.method == 'GET':
+        # todo = Todo.objects.get()
+       
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TodoSerializer(data=request.data,  instance=todo )
+        if serializer.is_valid():
+            serializer.save()
+        
+        
+      
+        
         
         
         
